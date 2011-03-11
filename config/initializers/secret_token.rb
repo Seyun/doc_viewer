569f7b3154b68085514e14fcd771176f7aaf48c7
Seyun Choi <ppassa@gmail.com>
2011-03-11 14:57:20 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocViewer::Application.config.secret_token = '2ff9d28abf60fb8486fd4e3787be1f5e8b8d08f4b12d99b95b520305713f8748bc055243002719d7c4833723bfbb6d272b7ff56c590b2fb4b2df184b44e5b3a2'
